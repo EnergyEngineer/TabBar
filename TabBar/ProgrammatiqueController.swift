fc9279649e58607ec006567085cda34cc2009bec
TOGNI Elie <Elie@macbook-pro-de-togni.home>
2018-08-02 21:50:51 +0200

    UIButton

--- TabBar/ProgrammatiqueController.swift
+++ TabBar/ProgrammatiqueController.swift
@@ -10,23 +10,29 @@ import UIKit
 
 class ProgrammatiqueController: UIViewController {
     
+    
+    // Définition des variables
     var monPremierUIView : UIView?
     var monPremierLabel: UILabel?
+    var monPremierBouton: UIButton?
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////// UIView ///////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        
         monPremierUIView = UIView(frame: CGRect(x: 30, y: 60, width: 50, height: 50))
         monPremierUIView?.backgroundColor = UIColor(red: 30/255, green: 123/255, blue: 90/255, alpha: 1)
         monPremierUIView?.layer.cornerRadius = 25
         
-        guard monPremierUIView != nil else {
-            return
-        }
+        guard monPremierUIView != nil else {return}
         
         view.addSubview(monPremierUIView!)
         print(monPremierUIView?.frame)
         print(monPremierUIView?.bounds)
-        
         
         let secondeVue = UIView(frame: monPremierUIView!.frame)
         secondeVue.center.y += 50
@@ -38,6 +44,10 @@ class ProgrammatiqueController: UIViewController {
         troisiemeVue.backgroundColor = UIColor.blue
         view.addSubview(troisiemeVue)
         
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////// UILabel///////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        
         monPremierLabel = UILabel(frame: CGRect(x: 0, y: monPremierUIView!.frame.maxY+20, width: view.frame.width, height: 50))
         monPremierLabel?.text="Coucou je suis un text"
         monPremierLabel?.numberOfLines = 1
@@ -46,8 +56,28 @@ class ProgrammatiqueController: UIViewController {
         monPremierLabel?.textAlignment = .center
         view.addSubview(monPremierLabel!)
         
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        ///////////////////////////////////////// UIButton//////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        
+        let rectDeMonBouton = CGRect(x: view.frame.width/2 - 75, y: monPremierLabel!.frame.maxY+20, width: 150, height: 40)
+        monPremierBouton = UIButton(frame: rectDeMonBouton)
+        monPremierBouton?.setTitle("Appuyer", for: UIControlState.normal)
+        monPremierBouton?.tintColor = UIColor.red
+        monPremierBouton?.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        monPremierBouton?.backgroundColor = UIColor.black
+        monPremierBouton?.layer.borderColor = UIColor.white.cgColor
+        monPremierBouton?.layer.borderWidth = 2
+        view.addSubview(monPremierBouton!)
+        
+        monPremierBouton?.addTarget(self, action: #selector(boutonAppuye), for: .touchUpInside)
+        
         
     }
+    
+    @objc func boutonAppuye(){
+        print("Tu as bien appuyé")
 
+    }
     
 }
