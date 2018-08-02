3cbc92c6800f65f08d7381398a65819d9fa0c2cc
TOGNI Elie <Elie@macbook-pro-de-togni.home>
2018-08-02 18:06:13 +0200

    Dernier code

--- TabBar/CycleEtFrameController.swift
+++ TabBar/CycleEtFrameController.swift
@@ -10,26 +10,42 @@ import UIKit
 
 class CycleEtFrameController: UIViewController {
 
+    // Pour appeler quelque chose une seule fois :
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("View did load appelé")
 
-        // Do any additional setup after loading the view.
     }
+    
+    // Effectuer une action lorsque la vue va apparaître
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("View will appear")
+    }
+    
+    // Effectuer un action lorsque la vue est apparue
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("View did appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("View will disappear")
+    }
+    
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("View did appear")
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
